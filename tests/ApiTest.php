f66ca3e4d5742c6afdb72529703b74637fb4e0a5
Tim Kleyersburg <tk@wacg.de>
2023-04-12 10:16:32 +0200

    chore: add tests for api

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -2,23 +2,18 @@
 
 namespace Tests;
 
-use Weaviate\Api\Batch;
-use Weaviate\Api\GraphQL;
-use Weaviate\Api\Objects;
-use Weaviate\Api\Schema;
-
-it('returns the schema api', function () {
-    expect(weaviate()->schema())->toBeInstanceOf(Schema::class);
+beforeEach(function () {
+    fakeResponse();
 });
 
-it('returns the objects api', function () {
-    expect(weaviate()->objects())->toBeInstanceOf(Objects::class);
-});
+it('can make a request with different methods', function (string $method) {
+    $response = weaviate()->api->{$method}('test');
 
-it('returns the batch api', function () {
-    expect(weaviate()->batch())->toBeInstanceOf(Batch::class);
-});
-
-it('returns the graphql api', function () {
-    expect(weaviate()->graphql())->toBeInstanceOf(GraphQL::class);
-});
+    expect($response->transferStats->getRequest()->getMethod())->toBe(strtoupper($method));
+})->with([
+    'get',
+    'post',
+    'put',
+    'patch',
+    'delete',
+]);
